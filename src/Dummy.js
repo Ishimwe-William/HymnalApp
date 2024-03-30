0f4cafcf4b396f02ed7f24d8510a79e71dcbc4a2
williamgitlab <william.ishimwe@ampersand.solar>
2024-03-30 20:28:14 +0200

    initial commit

---
+++ src/Dummy.js
@@ -0,0 +1,40 @@
+export const DummyHymns = [
+    {
+        id: 1,
+        hymnNumber: '1',
+        title: 'Murinzi Jy\' Uvuza Impanda',
+        stanzas: [
+            { id: 1, number: '1', text: 'Stanza 1' },
+            { id: 2, number: '2', text: 'Stanza 2' }
+        ],
+        refrains: [
+            { id: 1, number: '1', text: "Murinzi we vuz' impanda!" },
+        ]
+    },
+    {
+        id: 2,
+        hymnNumber: '2',
+        title: 'Umwami Ageze Kw\' Irembo',
+        stanzas: [
+            { id: 3, number: '1', text: 'Stanza 1' },
+            { id: 4, number: '2', text: 'Stanza 2' },
+            { id: 5, number: '3', text: 'Stanza 3' },
+        ],
+        refrains: [
+            { id: 2, number: '1', text: 'Kw\' irembo!' },
+        ]
+    },
+    {
+        id: 3,
+        hymnNumber: '3',
+        title: 'Nzabona Yesu mu maso',
+        stanzas: [
+            { id: 6, number: '1', text: 'Stanza 1' },
+            { id: 7, number: '2', text: 'Stanza 2' }
+        ],
+        refrains: [
+            { id: 3, number: '1', text: "Refrain 1" },
+            { id: 4, number: '2', text: "Refrain 2" },
+        ]
+    },
+]
